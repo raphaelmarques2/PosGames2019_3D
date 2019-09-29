08cb52b51154254e1ab6baa455f8bf64132c1746
Raphael Marques <jose.raphael.marques@gmail.com>
2019-09-29 11:27:17 -0300

    control force

---
+++ Assets/ControlForce/ControlForce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlForce : MonoBehaviour
+{
+    ConstantForce force;
+
+    public GameObject fire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        force = GetComponent<ConstantForce>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            force.force = new Vector3(0, 20, 0);
+            fire.SetActive(true);
+        }
+        else
+        {
+            force.force = new Vector3(0, 5, 0);
+            fire.SetActive(false);
+        }
+    }
+}
