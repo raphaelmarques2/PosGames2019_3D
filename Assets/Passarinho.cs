612dd8b7e3d52bd921d1f1c77c86f24d3ed1a9d9
Raphael Marques <jose.raphael.marques@gmail.com>
2019-10-05 16:28:10 -0300

    animações

---
+++ Assets/Passarinho.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passarinho : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        animator.SetFloat("Speed", v);
+
+        bool a = Input.GetKey(KeyCode.A);
+        animator.SetBool("Apontar", a);
+
+        if (Input.GetKeyDown(KeyCode.X))
+            animator.SetTrigger("Apontar2");
+
+    }
+}
